1464b273e7507701e7b355fb0f6593856bc9b570
Vitor Chollet <vitorchollet4@gmail.com>
2026-08-27 13:25:30 -0300

    Fix: enviando arquivos do projeto corretamente

---
+++ Between-Worlds/Livro.ts
@@ -0,0 +1,16 @@
+//classe
+export class Livro {
+//campos/atributos
+nome:string = "";
+descricão:string = "";
+preço:number = 0;
+Autores:string = "";
+DataPubli:string = "";
+Paginas:number = 0;
+Encadernacao:string = "";
+etária:string = "";
+conteúdo:string = "";
+ismbm:string = "";
+
+}
+//git push origin main
---
+++ Between-Worlds/main.ts
@@ -0,0 +1,41 @@
+
+    
+    
+let opcao: number;
+
+do {
+    console.log("\n===== LIVRARIA =====");
+    console.log("1 - Cadastro de produtos");
+    console.log("2 - Mostruário");
+    console.log("3 - Sistema de venda");
+    console.log("4 - Sistema de busca");
+    console.log("5 - Sair");
+
+    
+    opcao = Number(prompt("Escolha uma opção:"));
+
+    switch (opcao) {
+        case 1:
+            console.log("Cadastro de produtos");
+            break;
+
+        case 2:
+            console.log("Mostruário");
+            break;
+
+        case 3:
+            console.log("Sistema de venda");
+            break;
+
+        case 4:
+            console.log("Sistema de busca");
+            break;
+
+        case 5:
+            console.log("Saindo do sistema...");
+            break;
+
+        default:
+            console.log("Opção inválida!");
+    }
+} while (opcao !== 5);
